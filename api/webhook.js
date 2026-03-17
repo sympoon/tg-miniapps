f489ac5249d0e613c27ccb202da428bacc4e50b8
Arut <mcginley@mail.ru>
2026-03-17 15:47:21 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') return res.status(200).json({ ok: true });
 
     const BOT_TOKEN = process.env.BOT_TOKEN;
